95b8f3fea046d642d5245ce5c97d67e5f1107f30
Fabrice Macagno <fabrice.macagno@al.com.au>
2017-07-31 11:42:06 +1000

    Push dox only.

--- sync_scripts/push_docs_to_opensource.sh
+++ sync_scripts/push_docs_to_opensource.sh
@@ -15,41 +15,50 @@
 ################################################################################
 
 ### Update and push the wiki. ###
-internal_wiki_url=https://github.al.com.au/rnd/AL_USDMaya.wiki.git
-oss_wiki_url=https://github.al.com.au/fabricem/AL_USDMayaSyncTest_ext.wiki.git
+function update_and_push_wiki {
+  internal_wiki_url=https://github.al.com.au/rnd/AL_USDMaya.wiki.git
+  oss_wiki_url=https://github.al.com.au/fabricem/AL_USDMayaSyncTest_ext.wiki.git
 
-echo "Cloning iternal wiki."
-git clone -q $internal_wiki_url
-cd AL_USDMaya.wiki
-# Wiki repos have one branch by default, master. No need to checkout.
-echo "Pushing to opensource wiki."
-git push -q $oss_wiki_url master
-wiki_push_success=$?
-cd ..
-rm -rf AL_USDMaya.wiki
+  echo "Cloning iternal wiki."
+  git clone -q $internal_wiki_url
+  cd AL_USDMaya.wiki
+  # Wiki repos have one branch by default, master. No need to checkout.
+  echo "Pushing to opensource wiki."
+  git push -q $oss_wiki_url master
+  wiki_push_success=$?
+  cd ..
+  rm -rf AL_USDMaya.wiki
 
-if [ $wiki_push_success -ne 0 ]
-then
-	exit $wiki_push_success
-fi
-echo "Wiki has been successfully updated."
+  if [ $wiki_push_success -ne 0 ]
+  then
+      exit $wiki_push_success
+  fi
+  echo "Wiki has been successfully updated."
+}
 
 ### Update and push the doxygen documentation. ###
-internal_dox_url=https://github.al.com.au/documentation/AL_USDMaya.git
-oss_gh_url=https://github.al.com.au/fabricem/AL_USDMayaSyncTest_ext.git
+function update_and_push_dox {
+  internal_dox_url=https://github.al.com.au/documentation/AL_USDMaya.git
+  oss_gh_url=https://github.al.com.au/fabricem/AL_USDMayaSyncTest_ext.git
 
-echo "Cloning internal doxygen documentation."
-git clone -q $internal_dox_url
-cd AL_USDMaya
-# Our documentation repo has only one branch, gh-pages. No need to checkout.
-echo "Pushing to opensource doxygen documentation."
-git push -q $oss_gh_url gh-pages
-dox_push_success=$?
-cd ..
-rm -rf AL_USDMaya
+  echo "Cloning internal doxygen documentation."
+  git clone -q $internal_dox_url || exit $?
+  cd AL_USDMaya
+  # Our documentation repo has only one branch, gh-pages. No need to checkout
+  # the branch.
+  echo "Pushing to opensource doxygen documentation."
+  # Our internal process docs push process strips gh-pages history every time,
+  # retaining only the two last commits, that's why push if "forced".
+  git push -qf $oss_gh_url gh-pages
+  dox_push_success=$?
+  cd ..
+  rm -rf AL_USDMaya
 
-if [ $dox_push_success -ne 0 ]
-then
-	exit $dox_push_success
-fi
-echo "Doxygen documentation has been successfully updated."
+  if [ $dox_push_success -ne 0 ]
+  then
+      exit $dox_push_success
+  fi
+  echo "Doxygen documentation has been successfully updated."
+}
+
+update_and_push_dox
