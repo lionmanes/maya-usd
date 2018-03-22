88b03ef871c032708e9442e8df55923b92c408fb
Daniel Barry <danielbar@al.com.au>
2018-03-23 09:05:56 +1100

    Various small fixes

--- build_scripts/build_all_packages.sh
+++ build_scripts/build_all_packages.sh
@@ -1,8 +1,8 @@
 
 # run from the the AL_USDMaya directory
-cd AL_EventSystem; rez build --build-target RelWithDebInfo --variants 0 --install -- -- -j 46; if [ $? -ge 1 ]; then exit 1; fi ; cd -
-cd AL_MayaUtils; rez build --build-target RelWithDebInfo --variants 0 --install -- -- -j 46; if [ $? -ge 1 ]; then exit 1; fi ; cd -
-cd AL_USDUtils; rez build --build-target RelWithDebInfo --variants 0 --install -- -- -j 46; if [ $? -ge 1 ]; then exit 1; fi ; cd -
-cd AL_USDMayaUtils; rez build --build-target RelWithDebInfo --variants 0 --install -- -- -j 46; if [ $? -ge 1 ]; then exit 1; fi ; cd -
-rez build --build-target RelWithDebInfo --variants 0 --install -- -- -j 46
+cd AL_EventSystem; rez build --build-target RelWithDebInfo --install -- -- -j 46; if [ $? -ge 1 ]; then exit 1; fi ; cd -
+cd AL_MayaUtils; rez build --build-target RelWithDebInfo --install -- -- -j 46; if [ $? -ge 1 ]; then exit 1; fi ; cd -
+cd AL_USDUtils; rez build --build-target RelWithDebInfo --install -- -- -j 46; if [ $? -ge 1 ]; then exit 1; fi ; cd -
+cd AL_USDMayaUtils; rez build --build-target RelWithDebInfo --install -- -- -j 46; if [ $? -ge 1 ]; then exit 1; fi ; cd -
+rez build --build-target RelWithDebInfo --install -- -- -j 46
 
