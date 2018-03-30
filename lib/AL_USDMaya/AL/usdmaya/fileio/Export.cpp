0a62e399a89a09114973025713c0699a95e9242b
BuildBoy Jenkins <buildboy@al.com.au>
2018-03-30 22:48:24 +1100

    Squashed 'src/' changes from 7047c15a..b09a5cf4
    b09a5cf4 Updated CHANGELOG
    2b425d0e Updated version
    3af65208 fixes to the import of normal vectors (don't memcpy, since inputs are floats, outputs double)
    7ffaa0d0 removed dupe usdUtils lib
    d46942f1 update to wrap existing AL_USDMaya Singleton Cache
    d1863cc3 Use USD's singleton stage cache to keep a reference to our stage, and store the id on the proxyShape
    5e0005b9 Fix format note
    3755e011 Update document again
    5dc10dfd Update the document a bit
    85190009 Fix the crash issue
    7b882072 Pass all tests
    b50fe964 Inspecting crash issue
    46bc6318 Use simple mel to construct test
    00bfaeae Try to fix crash maya
    14729c0b Update and pass the test
    c2e0bb94 Update and try to pass test
    e6575761 Update test wip
    4502212a Add new test for new option
    593a9058 Update document on the new option
    cbff22e9 Use transform track as an option
    9356be9a Fix minor errors
    105e22e5 Try to clean up the copyAttributes a bit
    60641f03 Use binary search over std::lower_bound
    618efdfd Fix compile errors
    32619efb Fix more Robb note
    1d6ee7c3 Fix Robs notes wip
    71e67f1c Clean up code
    d40520b5 Reduce the duplicated isAnimated() checks
    734ab882 Revert to original MNodeClass approach
    67a15e79 Use even simpler way to get the static attributes
    c9729168 Reimplement using much simpler memory management
    2c3c680b Remove unused variable
    3a00c6a2 Fix the test crash issue
    5eaf3ea3 Merge to template into method since it is the only one that use it
    15295a87 Reset changes in export_constraints test
    de2da441 Remove unused variable
    06820893 Refine the memory management a bit
    ece62bdb Fix typo in attribute initialising
    d9c2672d Use attribute MObject instead of MString
    30c024d2 Swap the easy check to the front
    891e1b9e Update the doc comment
    819b1f4d Remove unused isNotWorld()
    b491103f Update test
    a21c4192 Switch to MDagPath approach and clean up
    4eeb6aa0 Fix compile error
    e488fcf3 Add comment
    d2d2ddb7 Use the new isAnimatedTransform
    a00d8d56 Implement the isAnimatedTransform()
    d3e94f85 Squash old approach of fix to one commit
    
    git-subtree-dir: src
    git-subtree-split: b09a5cf4180758bb3194cbed2eca4615a089fefe

--- lib/AL_USDMaya/AL/usdmaya/fileio/Export.cpp
+++ lib/AL_USDMaya/AL/usdmaya/fileio/Export.cpp
@@ -720,6 +720,7 @@ void Export::doExport()
   MObjectArray objects;
   const MSelectionList& sl = m_params.m_nodes;
   SdfPath defaultPrim;
+
   for(uint32_t i = 0, n = sl.length(); i < n; ++i)
   {
     MDagPath path;
@@ -818,22 +819,27 @@ MStatus ExportCommand::doIt(const MArgList& args)
   {
     AL_MAYA_CHECK_ERROR(argData.getFlagArgument("nc", 0, m_params.m_nurbsCurves), "ALUSDExport: Unable to fetch \"nurbs curves\" argument");
   }
+
   if(argData.isFlagSet("fr", &status))
   {
     AL_MAYA_CHECK_ERROR(argData.getFlagArgument("fr", 0, m_params.m_minFrame), "ALUSDExport: Unable to fetch \"frame range\" argument");
     AL_MAYA_CHECK_ERROR(argData.getFlagArgument("fr", 1, m_params.m_maxFrame), "ALUSDExport: Unable to fetch \"frame range\" argument");
     m_params.m_animation = true;
   }
-  else
-  if(argData.isFlagSet("ani", &status))
+  else if(argData.isFlagSet("ani", &status))
   {
     m_params.m_animation = true;
     m_params.m_minFrame = MAnimControl::minTime().value();
     m_params.m_maxFrame = MAnimControl::maxTime().value();
   }
+
   if (argData.isFlagSet("fs", &status))
   {
     AL_MAYA_CHECK_ERROR(argData.getFlagArgument("fs", 0, m_params.m_filterSample), "ALUSDExport: Unable to fetch \"filter sample\" argument");
+  }
+  if(argData.isFlagSet("eac", &status))
+  {
+    AL_MAYA_CHECK_ERROR(argData.getFlagArgument("eac", 0, m_params.m_extensiveAnimationCheck), "ALUSDExport: Unable to fetch \"extensive animation check\" argument");
   }
 
   if(m_params.m_animation)
@@ -920,6 +926,8 @@ MSyntax ExportCommand::createSyntax()
   status = syntax.addFlag("-fr", "-frameRange", MSyntax::kDouble, MSyntax::kDouble);
   AL_MAYA_CHECK_ERROR2(status, errorString);
   status = syntax.addFlag("-fs", "-filterSample", MSyntax::kBoolean);
+  AL_MAYA_CHECK_ERROR2(status, errorString);
+  status = syntax.addFlag("-eac", "-extensiveAnimationCheck", MSyntax::kBoolean);
   AL_MAYA_CHECK_ERROR2(status, errorString);
   syntax.enableQuery(false);
   syntax.enableEdit(false);
