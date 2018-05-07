ec21198aedd82ecf2c7b40275335423e8fec1a4e
aloysb <aloysb@al.com.au>
2018-05-07 14:05:24 +1000

    New patch for Maya 2019 PR92

--- AL_MayaUtils/package.py
+++ AL_MayaUtils/package.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
+
 name = 'AL_MayaUtils'
 
-version = '0.0.6'
+version = '0.0.7'
 
 private_build_requires = [
     'AL_CMakeLib',
     'cmake-2.8+',
     'AL_CMakeLibPython-6.0.9+<7',
-    'gcc-4.8',
+    'gcc-4.8.3',
     'AL_MTypeId-1.41+',
     'gdb-7.10',
     'doxygen-1',
@@ -16,19 +17,18 @@ private_build_requires = [
 ]
 
 requires = [
-    'stdlib-4.8',
+    'stdlib-4.8.3+<5',
     'Qt_vfx-5.6',
-    "AL_EventSystem-0.0.3+<0.1",
-    "AL_boost-1.55",
-    "AL_boost_python-1.55"]
-
-variants = [
-    ['CentOS-6.2+<7',
-     'mayaDevKit-2017.0'],
-    ['CentOS-6.2+<7',
-     'mayaDevKit-2018.0']
+    'AL_EventSystem-0.0.3+<0.1',
+    'AL_boost-1.55',
+    'AL_boost_python-1.55'
 ]
 
+variants = [
+    ['CentOS-6.2+<7', 'mayaDevKit-2017.0'],
+    ['CentOS-6.2+<7', 'mayaDevKit-2018.0'],
+    ['CentOS-6.6+<7', 'mayaDevKit-2019.b92']
+]
 
 def commands():
     prependenv('LD_LIBRARY_PATH', '{root}/lib')
--- AL_USDMayaUtils/package.py
+++ AL_USDMayaUtils/package.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
+
 name = 'AL_USDMayaUtils'
 
-version = '0.0.7'
+version = '0.0.8'
 
 private_build_requires = [
     'AL_CMakeLib',
     'cmake-2.8+',
-    'gcc-4.8',
+    'gcc-4.8.3',
     'gdb-7.10'
 ]
 
@@ -14,14 +15,13 @@ requires = [
     'AL_EventSystem-0.0.3+<0.1',
     'AL_MayaUtils-0.0.5+<0.1',
     'AL_USDUtils-0.0.6+<0.1',
-    'usdBase-0.8.al6',
+    'usdBase-0.8.al6'
 ]
 
 variants = [
-    ['CentOS-6.2+<7',
-     'mayaDevKit-2017.0'],
-    ['CentOS-6.2+<7',
-     'mayaDevKit-2018.0']
+    ['CentOS-6.2+<7', 'mayaDevKit-2017.0'],
+    ['CentOS-6.2+<7', 'mayaDevKit-2018.0'],
+    ['CentOS-6.6+<7', 'mayaDevKit-2019.b92']
 ]
 
 def commands():
