bf848160beef63a657ba634a3f4456929e50a662
BuildBoy Jenkins <buildboy@al.com.au>
2018-07-19 22:48:30 +1000

    Squashed 'src/' changes from 8ba568d7..10e550c8
    10e550c8 Fixed tests runtime environment for plugin translators
    00f3bcb5 updated new frame range translator to use the new API
    a8b3015b updated the documentation
    dc2a5f4e fix up build warning due to unused var
    5008186b fix windows build
    b2e732b9 fix mesh tests
    0bea98ec plugin translators refactoring first round
    6e4a9500 remove deprecated left handed option
    3004f325 fix windows build
    66641e31 fix mesh tests
    33417e58 plugin translators refactoring first round
    
    git-subtree-dir: src
    git-subtree-split: 10e550c8f67f3f956a71571cbbbdc456b64e5ba9

--- lib/AL_USDMaya/AL/usdmaya/fileio/translators/TranslatorBase.h
+++ lib/AL_USDMaya/AL/usdmaya/fileio/translators/TranslatorBase.h
@@ -28,12 +28,22 @@
 
 #include <iostream>
 #include <unordered_map>
+#include <functional>
 #include "AL/usdmaya/fileio/translators/TranslatorContext.h"
+#include "AL/usdmaya/fileio/ExportParams.h"
 
 namespace AL {
 namespace usdmaya {
 namespace fileio {
 namespace translators {
+
+enum class ExportFlag
+{
+  kNotSupported,
+  kFallbackSupport,
+  kSupported
+};
+
 
 //----------------------------------------------------------------------------------------------------------------------
 /// \brief  The base class interface of all translator plugins. The absolute minimum a translator plugin must implement
@@ -103,9 +113,13 @@ public:
   /// \param  parent a handle to an MObject that represents an AL_usd_Transform node. You should parent your DAG
   ///         objects under this node. If the prim you are importing is NOT a DAG object (e.g. surface shader, etc),
   ///         then you can ignore this parameter.
+  /// \param  output a handle to an MObject created in the importing process
   /// \return MS::kSuccess if all ok
-  virtual MStatus import(const UsdPrim& prim, MObject& parent)
+  virtual MStatus import(const UsdPrim& prim, MObject& parent, MObject& createdObj)
     { return MS::kSuccess; }
+
+  virtual UsdPrim exportObject(UsdStageRefPtr stage, MDagPath dagPath, const SdfPath& usdPath, const ExporterParams& params)
+    { return UsdPrim(); }
 
   /// \brief  If your node needs to set up any relationships after import (for example, adding the node to a set, or
   ///         making attribute connections), then all of that work should be performed here.
@@ -148,6 +162,10 @@ public:
   /// \return MS::kSuccess if all ok
   virtual MStatus update(const UsdPrim& prim)
     { return MStatus::kNotImplemented; }
+
+  /// \brief  Method used to test a Maya node to see whether it can be exported.
+  virtual ExportFlag canExport(const MObject& obj)
+    { return ExportFlag::kNotSupported; }
 
 };
 
@@ -219,6 +237,7 @@ private:
 };
 
 typedef TfRefPtr<TranslatorBase> TranslatorRefPtr;
+typedef std::vector<TranslatorRefPtr> TranslatorRefPtrVector;
 
 //----------------------------------------------------------------------------------------------------------------------
 /// \brief  Forms a registry of all plug-in translator types registered
@@ -228,6 +247,7 @@ class TranslatorManufacture
 {
 public:
   typedef TfRefPtr<TranslatorBase> RefPtr; ///< handle to a plug-in translator
+  typedef std::vector<RefPtr> RefPtrVector;
 
   /// \brief  constructs a registry of translator plugins that are currently registered within usd maya. This construction
   ///         should only happen once per-proxy shape.
@@ -236,10 +256,16 @@ public:
   TranslatorManufacture(TranslatorContextPtr context);
 
   /// \brief  returns a translator for the specified prim type.
-  /// \param  type_name the scheman name
+  /// \param  type_name the schema name
   /// \return returns the requested translator type
   AL_USDMAYA_PUBLIC
   RefPtr get(const TfToken type_name);
+
+  /// \brief  returns a translator for the specified prim type.
+  /// \param  type_name the schema name
+  /// \return returns the requested translator type
+  AL_USDMAYA_PUBLIC
+  RefPtr get(const MObject& mayaObject);
 
 private:
   std::unordered_map<std::string, TranslatorRefPtr> m_translatorsMap;
@@ -295,7 +321,7 @@ static RefPtr create(TranslatorContextPtr context);
 TfRefPtr<PlugClass>                                                             \
 PlugClass::create(TranslatorContextPtr context) {                               \
   TfType const &type = TfType::Find<TranslatedType>();                          \
-  if(!type.IsUnknown()) {                                                    \
+  if(!type.IsUnknown()) {                                                       \
     TfRefPtr<PlugClass> plugin = TfCreateRefPtr(new This());                    \
     plugin->setTranslatedType(type);                                            \
     plugin->setContext(context);                                                \
@@ -308,7 +334,7 @@ PlugClass::create(TranslatorContextPtr context) {                               
       typeid(TranslatedType).name());                                           \
     return TfNullPtr;                                                           \
   }                                                                             \
-}                                                                             \
+}                                                                               \
                                                                                 \
 TF_REGISTRY_FUNCTION(TfType)                                                    \
 {                                                                               \
