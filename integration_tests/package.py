7af31ad85f56a461b24b1b033782cf106a626325
Fabrice Macagno <fabrice.macagno@al.com.au>
2018-08-20 16:06:28 +1000

    Integration tests package

---
+++ integration_tests/package.py
@@ -0,0 +1,24 @@
+name = "AL_USDMayaIntegrationTests"
+
+def commands():
+    prependenv('PATH', '{root}/tests')
+
+
+tests = {
+    # AL_USDSchemas python module before AL_USDMaya in PYTHONPATH
+    'schemas_merge_schemas_first': {
+        'command': 'python -m unittest schemas_merge.test_merge',
+        'requires': [
+            'AL_USDMaya',
+            'AL_USDSchemas',
+        ]
+    },
+    # AL_USDMaya's schemas python module before AL_USDSchemas in PYTHONPATH
+    'schemas_merge_al_usdmaya_first': {
+        'command': 'python -m unittest schemas_merge.test_merge',
+        'requires': [
+            'AL_USDSchemas',
+            'AL_USDMaya',
+        ]
+    }
+}
