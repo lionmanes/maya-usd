73f2c0e1068d728870fa6cc722194ef51572979f
Rob Bateman <Rob.Bateman@al.com.au>
2019-10-23 10:21:38 +1100

    fix up correct version of AL_USDTransaction

--- package.py
+++ package.py
@@ -21,7 +21,7 @@ private_build_requires = [
 requires = [
     'usdBase-0.19.7',
     'usdImaging-0.19.7',
-    'AL_USDTransaction-0.1',
+    'AL_USDTransaction-0.2',
     'glew-2.0',
     'googletest-1.8',
     'python-2.7+<3',
